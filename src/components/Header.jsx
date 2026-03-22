37b43a593ca89af85a1ab1be8f88eb50f6e3c83f
tuanank5 <nguyentuananh20052005@gmail.com>
2026-03-22 14:51:02 +0000

    c

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import Header from "../components/Header";
 
 export default function Home() {
     const navigate = useNavigate();
 
     return (
         <div>
+            <Header />
             <button onClick={() => navigate("/login")}>
                 Login
             </button>
