3c282eb5b9911a5876b87a210b606e3cf90929a7
tuanank5 <nguyentuananh20052005@gmail.com>
2026-03-22 11:50:53 +0000

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Login from './components/Login';
 
 function App() {
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
     const navigate = useNavigate();
 
     return (
         <div>
-            <button></button>
+            <button onClick={() => navigate("/login")}>
+                Login
+            </button>
         </div>
     )
 }
